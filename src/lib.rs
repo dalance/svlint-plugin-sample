927bd4eac875fef993f115e31ee1ab9c32ce5436
dalance <dalance@gmail.com>
2023-05-12 14:33:47 +0900

    Update dependencies #2

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use sv_parser::{RefNode, SyntaxTree};
+use sv_parser::{NodeEvent, RefNode, SyntaxTree};
+use svlint::config::ConfigOption;
 use svlint::linter::{Rule, RuleResult};
 
 #[no_mangle]
@@ -10,9 +11,14 @@ pub extern "C" fn get_plugin() -> *mut dyn Rule {
 pub struct SamplePlugin;
 
 impl Rule for SamplePlugin {
-    fn check(&self, _syntax_tree: &SyntaxTree, node: &RefNode) -> RuleResult {
-        match node {
-            RefNode::InitialConstruct(_) => RuleResult::Fail,
+    fn check(
+        &mut self,
+        _syntax_tree: &SyntaxTree,
+        event: &NodeEvent,
+        _config: &ConfigOption,
+    ) -> RuleResult {
+        match event {
+            NodeEvent::Enter(RefNode::InitialConstruct(_)) => RuleResult::Fail,
             _ => RuleResult::Pass,
         }
     }
@@ -21,7 +27,7 @@ impl Rule for SamplePlugin {
         String::from("sample_plugin")
     }
 
-    fn hint(&self) -> String {
+    fn hint(&self, _config: &ConfigOption) -> String {
         String::from("`initial` is forbidden")
     }
 
